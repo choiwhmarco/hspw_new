88015424a240e6c5fb9d66f78f1f4e9eaacf5d17
Marco Choi <whchoi@ucsd.edu>
2023-01-10 11:23:35 -0800

    updated basic-card.js

--- src/components/basic-card.js
+++ src/components/basic-card.js
@@ -22,88 +22,12 @@ import './basic-card.css';
 			Defaults to ''.
 */
 
- const basicCards = [
-    {
-      imageSrc: iconFolder + 'icon-salivary-protein.png',
-      title: 'Protein Search',
-      blurb: 'Search for specific salivary proteins found in our database.'
-    },
-    {
-      imageSrc: iconFolder + 'icon-analyze.png',
-      title: 'Protein Analysis',
-      blurb: 'Evaluate proteins, quantify abundance and perform statistics.'
-    },
-    {
-      imageSrc: iconFolder + 'icon-upload.png',
-      title: 'Upload Experiment',
-      blurb: 'Upload experiment to the database. Files have to be in mzTab format.'
-    },
-    {
-      imageSrc: iconFolder + 'icon-download.png',
-      title: 'Download Datasets',
-      blurb: 'Download datasets from database with protein abundance and sequence.'
-    },
-    {
-      imageSrc: iconFolder + 'icon-pubmed.png',
-      title: 'PubMed',
-      blurb: 'Includes links to full text articles and other related resources.'
-    },
-    {
-      imageSrc: iconFolder + 'icon-gene.png',
-      title: 'Find Gene',
-      blurb: 'A locatable region of genomic sequence, corresponding to a unit of inheritance.'
-    },
-    {
-      imageSrc: iconFolder + 'icon-clustering.png',
-      title: 'Cluster Search',
-      blurb: 'Search clusters that share one or more common proteins are merged further.'
-    },
-    {
-      imageSrc: iconFolder + 'icon-api.png',
-      title: 'API',
-      blurb: 'Allows access to the datasets by retrieving requested data in JSON format.'
-    },
-    {
-      size: '2',
-      destination: false,
-      rawContent:
-        <div className='basic-card-content basic-card-content-centered'>
-          <h4>Statistics</h4>
-          <ul className="bulletless">
-            <li>7 Contributing Institutions</li>
-            <li>7 Studies</li>
-            <li>946 Datasets</li>
-            <li>5 Tissue Types</li>
-            <li>3 Diseases + Healthy Controls</li>
-          </ul>
-        </div>
-    },
-    {
-      size: '2',
-      destination: false,
-      rawContent: 
-        <div className="basic-card-content basic-card-content-centered embed-responsive" style={{height: '300px'}}>
-          <iframe className="embed-responsive-item" src="https://youtube.com/embed/u4JN1FmLGE4" width="100%"></iframe>
-        </div>
-    },
-    {
-      size: '4',
-      destination: false,
-      rawContent:
-        <div className='basic-card-content basic-card-content-centered'>
-          <h4>Recent News</h4>
-          <ul className="bulletless">
-            <li>Bookmark our new URL: <a href="https://salivaryproteome.org/">https://salivaryproteome.org/</a></li>
-            <li>Our new JCVI covid-19 data is available</li>
-          </ul>
-        </div>
-    }
-  ];
 class BasicCard extends React.Component {
-    
+  
 	render() {
-		return (
-			<div className="basic-card" data-size={this.props.size} data-destination={destination}>
+    let content = this.props.rawContent;
+		if(!content) {
+			content = 
 				<div className='basic-card-content'>
 					<div className="basic-card-image-wrapper">
 						<img src={this.props.imageSrc} />
@@ -111,6 +35,15 @@ class BasicCard extends React.Component {
 					<h4>{ this.props.title }</h4>
 					<p>{ this.props.blurb }</p>
 				</div>
+			;
+		}
+		let destination = this.props.destination;
+		if(destination !== false) {
+			destination = true;
+		}
+		return (
+			<div className="basic-card" data-size={this.props.size} data-destination={destination}>
+				{ content }
 			</div>
 		)
 	};
